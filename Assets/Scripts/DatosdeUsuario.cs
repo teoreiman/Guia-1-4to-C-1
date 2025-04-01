3317f36bb6ac05e01aec8fb0532f56bdcb85dde2
teoreiman <teo@reiman.com.ar>
2025-04-01 09:04:38 -0300

    guiaa

---
+++ Assets/Scripts/DatosdeUsuario.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosdeUsuario : MonoBehaviour
+{
+    public string nombreUsuario;
+    public int edadUsuario;
+    public float alturaUsuario;
+    public bool esDonante;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombreUsuario + "tengo " + edadUsuario + "y mido " + alturaUsuario + "Mts");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
